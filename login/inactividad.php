37d6d8e6b95905f959279b956b95409287e78247
diegoteos <diegoteos@hotmail.com>
2022-09-06 23:14:14 -0600

    Cierre de sesion por inactividad

--- login/inactividad.php
+++ login/inactividad.php
@@ -1,29 +1,15 @@
 <?php
-/*
-
-if (isset($_SESSION['start']) && (time() - $_SESSION['start'] > 10)) {
-    session_unset();
-    session_destroy();
-    echo "Sesion finalizada";
-?>
-    <META HTTP-EQUIV="REFRESH" CONTENT="12;URL=login.php">
-<?php
-}
-$_SESSION['start'] = time();
-*/
-
 
 if (!isset($_SESSION['tiempo'])) {
-    $_SESSION['tiempo']=time();
-}
-else if (time() - $_SESSION['tiempo'] > 500) {
+    $_SESSION['tiempo'] = time();
+} else if (time() - $_SESSION['tiempo'] > 500) {
     session_destroy();
-    ?>
+?>
     <META HTTP-EQUIV="REFRESH" CONTENT="0;URL=login.php">
-    <?php 
-    die();  
+<?php
+    die();
 }
-$_SESSION['tiempo']=time();
+$_SESSION['tiempo'] = time();
 
 
 
