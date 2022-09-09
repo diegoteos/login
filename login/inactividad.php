4ec9ce23615d5294b648476d6b1df5f1e057edcf
diegoteos <diegoteos@hotmail.com>
2022-09-09 13:03:38 -0600

    comentarios

--- login/inactividad.php
+++ login/inactividad.php
@@ -1,16 +1,10 @@
 <?php
 
-if (!isset($_SESSION['tiempo'])) {
-    $_SESSION['tiempo'] = time();
-} else if (time() - $_SESSION['tiempo'] > 500) {
+if (!isset($_SESSION['tiempo'])) {  // validamos si existe la session tiempo y si no existe la creamos
+    $_SESSION['tiempo'] = time();   // creamos la sesion de tiempo asignandole el valor de la funcion time()
+} else if (time() - $_SESSION['tiempo'] > 300) {    // si la session tiempo existe entonces vemos si es mayor de 3 minutos y si es asi la destruimos
     session_destroy();
-?>
-    <META HTTP-EQUIV="REFRESH" CONTENT="0;URL=login.php">
-<?php
+    header("location: login.php");
     die();
 }
-$_SESSION['tiempo'] = time();
-
-
-
-?>
+$_SESSION['tiempo'] = time(); // Reseteamos la session de tiempo cada vez que interactuamos con el archivo
